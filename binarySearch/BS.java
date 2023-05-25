dbab84d4813a665e25e135c877caceeb26496143
IreneYIN7 <yinzhebinirene7@gmail.com>
2023-05-25 16:53:48 -0400

    2d bs

--- binarySearch/BS.java
+++ binarySearch/BS.java
@@ -32,6 +32,37 @@ public class BS {
     // 3 4 5
     //   m
     // 5
+
+    /**
+     * Given a 2D matrix that contains integers only, which each row is sorted in an ascending order. 
+     * The first element of next row is larger than (or equal to) the last element of previous row.
+     * Given a target number, returning the position that the target locates within the matrix. 
+     * If the target number does not exist in the matrix, return {-1, -1}.
+     */
+    public int[] search(int[][] matrix, int target) {
+        // Write your solution here
+        // corner case:
+        if(matrix.length == 0 && matrix[0].length == 0) return new int[]{-1,-1};
+        //    0  1  2
+        // 0  1, 2, 3
+        // 1  4, 5, 7
+        // 2  8, 9, 10
+        int rowLength = matrix[0].length;
+        int front = 0;
+        int end = matrix.length* rowLength -1;
+        while(front <= end){
+          int midEle = (end - front) / 2 + front;
+          int midRow = midEle / rowLength;
+          int midCol = midEle % rowLength;
+          if(matrix[midRow][midCol] == target) return new int[]{midRow, midCol};
+          else if(matrix[midRow][midCol] > target) end = midEle - 1;
+          else front = midEle + 1;
+        }
+        return new int[]{-1, -1};
+    
+      }
+      // TC: O(log(m*n))
+      // SC: O(1)
     
     public static void main(String args[]){
         BS solu = new BS();
