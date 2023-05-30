968dc83e18f18929cd115c48908d874c39da79e9
IreneYIN7 <yinzhebinirene7@gmail.com>
2023-05-29 23:27:27 -0400

    folyd cycle

---
+++ LinkedList/detectCycle.java
@@ -0,0 +1,16 @@
+package LinkedList;
+
+public class detectCycle {
+    public class ListNode {
+        public int value;
+        public ListNode next;
+        public ListNode(int value) {
+            this.value = value;
+            next = null;
+        }
+    }
+    public ListNode detectCycle(ListNode head){
+        
+    }
+    
+}
