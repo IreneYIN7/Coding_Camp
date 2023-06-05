0f06c74246665f0c41daaba6a1724addeb182d55
IreneYIN7 <yinzhebinirene7@gmail.com>
2023-06-05 17:12:25 -0400

    detectCycle

--- LinkedList/detectCycle.java
+++ LinkedList/detectCycle.java
@@ -15,8 +15,26 @@ public class detectCycle {
         // (a-b)C -- 整数圈 for slow pointer. 
         // then let slow start from P and another pointer start from the start Point, 
         // when they meet, it's the start of the cycle 
+        // corner case:
+        if(head == null || head.next == null) return null;
+        // find the point
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast) break;
+        }
+        if(fast == null || fast.next == null) return null;
 
-        
+        ListNode result = head;
+        while(result != slow){
+            slow = slow.next;
+            result = result.next;
+        }
+
+        return result;
+
     }
     
 }
